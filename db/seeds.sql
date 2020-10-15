fef571af60e78599cde72617f769c10b847b3b7f
Steven Bong <stevenseal99@yahoo.com>
2020-10-15 15:35:56 -0700

    Change the size of button to make all same size. Changed the structure of layout in the index.handlebars

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,17 +2,17 @@
 INSERT INTO burgers (burger_name) 
 VALUES ("Hatch Green Chile Bacon Burger");
 
-INSERT INTO burgers (burger_name, devoured) 
-VALUES ("Triple Meat Whataburger", true);
+INSERT INTO burgers (burger_name) 
+VALUES ("Triple Meat Whataburger");
 
-INSERT INTO burgers (burger_name, devoured) 
-VALUES ("Avocado Bacon Burger", true);
+INSERT INTO burgers (burger_name) 
+VALUES ("Avocado Bacon Burger");
 
 INSERT INTO burgers (burger_name) 
 VALUES ("Whataburger");
 
-INSERT INTO burgers (burger_name, devoured) 
-VALUES ("Jalapeno & Cheese Whataburger", true);
+INSERT INTO burgers (burger_name) 
+VALUES ("Jalapeno & Cheese Whataburger");
 
 INSERT INTO burgers (burger_name) 
 VALUES ("Whataburger Jr.");
