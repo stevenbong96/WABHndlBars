8c8fb8e7968b70f8f900121aaac255ebbf2625b8
Steven Bong <stevenseal99@yahoo.com>
2020-10-14 18:18:52 -0700

    Worked on all the files from back-end to front-end based on reference from class activity. Managed to display the front-end, but still need working on the back-end things

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+-- Insert values
+INSERT INTO burgers (burger_name) 
+VALUES ("Hatch Green Chile Bacon Burger");
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Triple Meat Whataburger", true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Avocado Bacon Burger", true);
+
+INSERT INTO burgers (burger_name) 
+VALUES ("Whataburger");
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Jalapeno & Cheese Whataburger", true);
+
+INSERT INTO burgers (burger_name) 
+VALUES ("Whataburger Jr.");
+
+INSERT INTO burgers (burger_name) 
+VALUES ("Bacon & Cheese Whataburger");
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Double Meat Whataburger", true);
