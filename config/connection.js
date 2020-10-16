78495e7a2dab473a0186b1be938ad0c9976a9a10
Steven Bong <stevenseal99@yahoo.com>
2020-10-16 08:37:06 -0700

    Added JAWSDB_URL connection to deploy to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+let connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Joseph96-",
-  database: "burgers_db"
-});
+// Create conditional statement to deploy sql to heroku
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Joseph96-",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
